7ae08b8f113bafe62727e64a73aa115513e96c4d
Michael Monsour <michael@lethek.net>
2016-03-01 16:38:31 +1000

    bumped version number

--- Flurl/Properties/AssemblyInfo.cs
+++ Flurl/Properties/AssemblyInfo.cs
@@ -3,4 +3,5 @@
 [assembly: AssemblyTitle("Flurl")]
 [assembly: AssemblyDescription("A fluent, portable URL builder.")]
 [assembly: AssemblyCopyright("Copyright © Todd Menier 2015")]
-[assembly: AssemblyVersion("1.0.10")]
+[assembly: AssemblyVersion("1.0.11")]
+[assembly: AssemblyInformationalVersion("1.0.11-airtime001")]
